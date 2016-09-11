46f24315173b2c5cd8476ad22cb098f122a014ca
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-11 19:10:29 +0900

    wip

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 echo create database isucon5q | mysql -uroot
 mysql -uroot < sql/schema.sql
+
 cd script
 bundle install -j4
-bundle exec ruby gen_users >/dev/null
-bundle exec ruby gen_relations >/dev/null
-bundle exec ruby gen_entries >/dev/null
-bundle exec ruby gen_comments >/dev/null
-bundle exec ruby gen_footprints >/dev/null
+bundle exec ruby gen_users.rb >/dev/null
+bundle exec ruby gen_relations.rb >/dev/null
+bundle exec ruby gen_entries.rb >/dev/null
+bundle exec ruby gen_comments.rb >/dev/null
+bundle exec ruby gen_footprints.rb >/dev/null
 bundle exec ruby check_testsets.rb
