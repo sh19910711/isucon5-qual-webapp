6a587616f454925f90416bb9e1a6a020d353431b
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-11 19:12:32 +0900

    wip

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ mysql -uroot < sql/schema.sql
 
 cd script
 bundle install -j4
-bundle exec ruby gen_users.rb >/dev/null
-bundle exec ruby gen_relations.rb >/dev/null
-bundle exec ruby gen_entries.rb >/dev/null
-bundle exec ruby gen_comments.rb >/dev/null
-bundle exec ruby gen_footprints.rb >/dev/null
+bundle exec ruby gen.rb gen_users >/dev/null
+bundle exec ruby gen.rb gen_relations >/dev/null
+bundle exec ruby gen.rb gen_entries >/dev/null
+bundle exec ruby gen.rb gen_comments >/dev/null
+bundle exec ruby gen.rb gen_footprints >/dev/null
 bundle exec ruby check_testsets.rb
